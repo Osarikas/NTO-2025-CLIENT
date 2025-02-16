dd23fa92fe8a7bf7e9ae4d9385784a07b76c04b4
Osarikas <yastruckov@yandex.ru>
2025-02-16 20:05:40 +0300

    style: Шрифты

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,12 @@
 plugins {
-    kotlinAndroid
     androidApplication
     jetbrainsKotlinSerialization version Version.Kotlin.language
     kotlinAnnotationProcessor
     id("com.google.dagger.hilt.android") version("2.51.1")
+    alias(libs.plugins.kotlin.android)
 }
 
 val packageName = "ru.myitschool.work"
-
 android {
     namespace = packageName
     compileSdk = 35
@@ -37,40 +36,38 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.activity:activity-ktx:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.annotation)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     defaultLibrary()
 
     implementation(Dependencies.AndroidX.activity)
     implementation(Dependencies.AndroidX.fragment)
     implementation(Dependencies.AndroidX.constraintLayout)
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.7")
-
-    implementation(Dependencies.Retrofit.library)
-    implementation(Dependencies.Retrofit.gsonConverter)
-    implementation("io.ktor:ktor-client-content-negotiation:3.0.3")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:3.0.3")
-    implementation("io.ktor:ktor-client-cio:3.0.3")
-    implementation("io.ktor:ktor-client-core:3.0.3")
-    implementation("io.ktor:ktor-client-serialization:2.3.7")
-    implementation("com.squareup.picasso:picasso:2.8")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
-    implementation("androidx.datastore:datastore-preferences:1.1.2")
-    implementation("com.google.mlkit:barcode-scanning:17.3.0")
-
-    val cameraX = "1.4.1"
-    implementation("androidx.camera:camera-core:$cameraX")
-    implementation("androidx.camera:camera-camera2:$cameraX")
-    implementation("androidx.camera:camera-lifecycle:$cameraX")
-    implementation("androidx.camera:camera-view:$cameraX")
-    implementation("androidx.camera:camera-mlkit-vision:1.4.1")
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.serialization)
+    implementation(libs.picasso)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.barcode.scanning)
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.mlkit.vision)
 
     val hilt = "2.51.1"
-    implementation("com.google.dagger:hilt-android:$hilt")
+    implementation(libs.hilt.android)
     kapt("com.google.dagger:hilt-android-compiler:$hilt")
 }
 
